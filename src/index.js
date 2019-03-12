7838bcaa8991045e71678904466e69050519f794
999van666 <paganec666.van@gmail.com>
2019-03-13 01:48:53 +0300

    feat:cool all my manye

--- src/index.js
+++ src/index.js
@@ -1,5 +1,57 @@
-// PLEASE DON'T change function name
+    
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+
+    const manye = {};
+          
+    if (currency >= 10000) {
+        manye.error = "You are rich, my friend! We don't have so much coins for exchange";
+      return manye;
+    }
+  
+    if (currency >= 50) {
+        manye.H = 0;
+  
+      while (currency >= 50) {
+        manye.H++;
+        currency -= 50;
+      } 
+    }
+  
+    if (currency >= 25) {
+        manye.Q = 0;
+  
+      while (currency >= 25) {
+        manye.Q++;
+        currency -= 25;
+      }   
+    }
+  
+    if (currency >= 10) {
+        manye.D = 0;
+  
+      while (currency >= 10) {
+        manye.D++;
+        currency -= 10;
+      }
+    }
+  
+    if (currency >= 5) {
+        manye.N = 0;
+  
+      while (currency >= 5) {
+        manye.N++;
+        currency -= 5;
+      }
+    }
+      
+    if (currency >= 1) {
+        manye.P = 0;
+  
+      while (currency >= 1) {
+        manye.P++;
+        currency -= 1;
+      }
+    }
+    
+  return manye;       
+  }
